5c987b65f47c5198461287d79f661fac1d7cafc9
ducphamle2 <ducphamle212@gmail.com>
2023-06-29 14:37:09 +0700

    finished test demo

--- packages/rpc-sync/tests/index.spec.ts
+++ packages/rpc-sync/tests/index.spec.ts
@@ -1,5 +1,21 @@
+import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
+import { SyncData } from '../src/index';
+
 describe('foobar', () => {
-  it('foo', () => {
+  it('foo', async () => {
     console.log('foo');
+    const tendermintSpy = jest.spyOn(Tendermint34Client, 'connect');
+    tendermintSpy.mockImplementation((endpoint): any => {
+      const obj = {
+        txSearch: () => {
+          return {
+            txs: [],
+            totalCount: 1
+          };
+        }
+      };
+      return obj;
+    });
+    const sync = new SyncData({ rpcUrl: '', queryTags: [] });
   });
 });
