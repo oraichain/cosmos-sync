f5636924d7940e1025b34e245dfd3d4f29fb1870
Pham Tu <tubackkhoa@gmail.com>
2023-06-29 11:03:17 +0700

    refractor

--- packages/rpc-sync/src/index.ts
+++ packages/rpc-sync/src/index.ts
@@ -1,7 +1,6 @@
-import { Tendermint34Client } from '@cosmjs/tendermint-rpc';
+import { Tendermint34Client, Event as RpcEvent } from '@cosmjs/tendermint-rpc';
 import { buildQuery } from '@cosmjs/tendermint-rpc/build/tendermint37/requests';
 import { QueryTag } from '@cosmjs/tendermint-rpc/build/tendermint37';
-import { Event as RpcEvent } from '@cosmjs/tendermint-rpc';
 import { Event } from 'cosmjs-types/tendermint/abci/types';
 import { Readable, Writable } from 'stream';
 
@@ -18,98 +17,61 @@ export type Txs = {
 
 export abstract class WriteData extends Writable {
   constructor() {
-    super();
+    super({ objectMode: true });
   }
 
   async _write(chunk: any, encoding: BufferEncoding, callback: (error?: Error) => void): Promise<void> {
+    let success = false;
     try {
-      await this.process(chunk);
+      success = await this.process(chunk);
     } catch (error) {
       console.log('error writing data: ', error);
     } finally {
-      callback();
+      if (success) callback();
     }
   }
 
-  abstract process(chunk: any): any;
+  abstract process(txs: Txs): Promise<boolean>;
 }
 
+export type SyncDataOptions = {
+  queryTags: QueryTag[];
+  rpcUrl: string;
+  offset?: number;
+  limit?: number;
+  interval?: number;
+};
+
 export abstract class SyncData extends Readable {
-  private _queryTags: QueryTag[];
-  private _rpcUrl: string;
-  private _interval: number;
-  public offset: number;
-  public limit: number;
-  constructor({
-    queryTags,
-    rpcUrl,
-    offset = 1,
-    limit = 100,
-    interval = 5000
-  }: {
-    queryTags: QueryTag[];
-    rpcUrl: string;
-    offset?: number;
-    limit?: number;
-    interval?: number;
-  }) {
+  constructor(public readonly options: SyncDataOptions) {
     super();
-    this._rpcUrl = rpcUrl;
-    this._queryTags = queryTags;
-    this.offset = offset;
-    this.limit = limit;
-    this._interval = interval;
   }
+
   _read() {
     this.queryTendermint();
   }
 
-  private convertEventAttrsToAscii(events: readonly RpcEvent[]): Event[] {
-    return events.map((event) => ({
-      ...event,
-      attributes: event.attributes.map((attr) => ({
-        index: (attr as any).index,
-        key: Buffer.from(attr.key).toString('ascii'),
-        value: Buffer.from(attr.value).toString('ascii')
-      }))
-    }));
-  }
-
   private async queryTendermint() {
-    const tendermint = await Tendermint34Client.connect(this._rpcUrl);
-    const query = buildQuery({ tags: this._queryTags });
+    const tendermint = await Tendermint34Client.connect(this.options.rpcUrl);
+    const query = buildQuery({ tags: this.options.queryTags });
     while (true) {
       try {
         const result = await tendermint.txSearch({
           query,
-          page: this.offset,
-          per_page: this.limit
+          page: this.options.offset,
+          per_page: this.options.limit
         });
         const storedResults = result.txs.map((tx) => ({
           ...tx,
           hash: Buffer.from(tx.hash).toString('hex').toUpperCase(),
-          events: this.convertEventAttrsToAscii(tx.result.events)
+          events: tx.result.events
         }));
-        await new Promise((resolve) => setTimeout(resolve, this.interval));
-        await this.query({ txs: storedResults, total: result.totalCount });
+        await new Promise((resolve) => setTimeout(resolve, this.options.interval));
+        this.push({ txs: storedResults, total: result.totalCount });
       } catch (error) {
         console.log('error query tendermint tx search: ', error);
         // only returns if search successfully
       }
     }
-  }
-
-  abstract query(txs: Txs): any;
-
-  get queryTags() {
-    return this._queryTags;
-  }
-
-  get rpcUrl() {
-    return this._rpcUrl;
-  }
-
-  get interval() {
-    return this._interval;
   }
 }
