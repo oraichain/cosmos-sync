8b868d9bcffe5c6e5bc4606e015eeb967c1643be
trungbach <trunga2k29@gmail.com>
2024-11-13 15:02:27 +0700

    chore: fixed error parse event from rpc

--- packages/rpc-sync/src/helpers.ts
+++ packages/rpc-sync/src/helpers.ts
@@ -1,5 +1,5 @@
-import { DecodedTxRaw, decodeTxRaw } from "@cosmjs/proto-signing";
-import { TxEvent, Event } from "@cosmjs/tendermint-rpc/build/tendermint37";
+import { DecodedTxRaw, decodeTxRaw } from '@cosmjs/proto-signing';
+import { TxEvent, Event } from '@cosmjs/tendermint-rpc/build/tendermint37';
 
 // TxEvent Parsing
 export interface ParsedTxEvent {
@@ -7,27 +7,26 @@ export interface ParsedTxEvent {
   tx: DecodedTxRaw;
   height: number;
   result: {
-      events: Event[];
-      code: number;
-      codespace?: string;
-      log?: string;
-      data?: Uint8Array;
-      gasWanted: number;
-      gasUsed: number;
+    events: Event[];
+    code: number;
+    codespace?: string;
+    log?: string;
+    data?: Uint8Array;
+    gasWanted: number;
+    gasUsed: number;
   };
 }
 
-
-export function parseTxEvent(txEvent: TxEvent):ParsedTxEvent {
+export function parseTxEvent(txEvent: TxEvent): ParsedTxEvent {
   return {
     hash: Buffer.from(txEvent.hash).toString('hex').toLocaleUpperCase(),
     tx: decodeTxRaw(txEvent.tx),
     height: txEvent.height,
     result: {
       ...txEvent.result,
-      events: parseRpcEvents(txEvent.result.events)
+      events: [...txEvent.result.events]
     }
-  }
+  };
 }
 
 export function isBase64(str: string) {
@@ -45,12 +44,12 @@ export const parseRpcEvents = (events: readonly Event[]): Event[] => {
       let obj;
       try {
         obj = {
-          key: isBase64(attr.key) ? Buffer.from(attr.key, "base64").toString("utf-8") : attr.key,
-          value: isBase64(attr.value) ? Buffer.from(attr.value, "base64").toString("utf-8") : attr.value
+          key: isBase64(attr.key) ? Buffer.from(attr.key, 'base64').toString('utf-8') : attr.key,
+          value: isBase64(attr.value) ? Buffer.from(attr.value, 'base64').toString('utf-8') : attr.value
         };
       } catch (err) {
         obj = {
-          key: isBase64(attr.key) ? Buffer.from(attr.key, "base64").toString("utf-8") : attr.key,
+          key: isBase64(attr.key) ? Buffer.from(attr.key, 'base64').toString('utf-8') : attr.key,
           value: attr.value
         };
       }
